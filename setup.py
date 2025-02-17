cce524eb27053bc9865faeb808de0f2c297bfe6a
Fouad Khelil <165522876+Fouad-Khelil@users.noreply.github.com>
2025-02-17 18:58:19 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pysecdeserializer",
+    version="0.1.0",
+    description="A package to deserialize SEC EDGAR .nc filings",
+    author="Mohamed Miloudi",
+    author_email="m.miloudi1357@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
